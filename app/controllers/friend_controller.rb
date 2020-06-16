ff90e5ecb795b00c15999e1ff4205a1061e46404
nisino25 <nisino25@gmail.com>
2020-06-16 01:18:20 -0700

    Near pin

--- app/controllers/friend_controller.rb
+++ app/controllers/friend_controller.rb
@@ -2,6 +2,15 @@ class FriendController < ApplicationController
 
     def index
         @others = User.all
+
+        # @others.each do |other|
+        #     other.songs.each |song|
+        #         @counts = 0
+        #         if current_user.songs.exists?(url: song.url)
+        #             @counts = @counts + 1
+        #         end
+        #     end
+        # end
     end
 
     def following
--- app/models/practice.rb
+++ app/models/practice.rb
@@ -12,4 +12,8 @@ doc = Nokogiri::HTML(open('https://www.ufret.jp/song.php?data=44896'))
 # puts doc.xpath(" //h1 [@style= 'margin-top:7px;margin-bottom:5px;margin-left:2px;']").children
 # puts doc.xpath(" //a [@class= 'list-group-item list-group-item-action normal-chord']/strong").children
 
-puts doc.title.to_s.slice(0..(doc.title.index('/')))[0..-3]
+doc =  doc.title.to_s.slice(0..(doc.title.index('/')))[0..-3]
+
+@user = User.first
+@user2 = User.find(2)
+puts @user.songs.count
